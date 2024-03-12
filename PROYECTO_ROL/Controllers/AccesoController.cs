cc5dc640e9493733f3e8467727c9ec88e325fb58
Che <profesor76@hotmail.com>
2024-03-12 14:19:27 -0600

    120324

--- PROYECTO_ROL/Controllers/AccesoController.cs
+++ PROYECTO_ROL/Controllers/AccesoController.cs
@@ -1,4 +1,9 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Mvc;
+using PROYECTO_ROL.Models;
+using System.Security.Claims;
+using static PROYECTO_ROL.Data.DA_Logica;
 
 namespace PROYECTO_ROL.Controllers
 {
@@ -7,6 +12,54 @@ namespace PROYECTO_ROL.Controllers
         public IActionResult Index()
         {
             return View();
+        }
+
+        //USAR REFERENCIAS Models y Data
+        [HttpPost]
+        public async Task<IActionResult> Index(Usuario _usuario)
+        {
+            DA_Usuario _da_usuario = new DA_Usuario();
+
+            var usuario = _da_usuario.ValidarUsuario(_usuario.Correo, _usuario.Clave);
+
+            if (usuario != null)
+            {
+
+                //2.- CONFIGURACION DE LA AUTENTICACION
+                #region AUTENTICACTION
+                var claims = new List<Claim>
+                {
+                    new Claim(ClaimTypes.Name, usuario.Nombre),
+                    new Claim("Correo", usuario.Correo),
+                };
+                foreach (string rol in usuario.Roles)
+                {
+                    claims.Add(new Claim(ClaimTypes.Role, rol));
+                }
+                var claimsIdentity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+
+                await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, new ClaimsPrincipal(claimsIdentity));
+                #endregion
+
+
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                return View();
+            }
+
+        }
+
+        public async Task<IActionResult> Salir()
+        {
+            //3.- CONFIGURACION DE LA AUTENTICACION
+            #region AUTENTICACTION
+            await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+            #endregion
+
+            return RedirectToAction("Index");
+
         }
     }
 }
