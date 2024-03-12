35926464fbc757ffe8100e603971b1e6c4571777
Che <profesor76@hotmail.com>
2024-03-12 13:38:18 -0600

    Agregar archivos de proyecto.

---
+++ PROYECTO_ROL/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PROYECTO_ROL.Models;
+using System.Diagnostics;
+
+namespace PROYECTO_ROL.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Ventas()
+        {
+            return View();
+        }
+        public IActionResult Compras()
+        {
+            return View();
+        }
+        public IActionResult Clientes()
+        {
+            return View();
+        }
+
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
